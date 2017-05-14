4c0cbe2f51dadd9e5cb1e7882b550bbdd0f9da2b
mtumartian <mrmartin@mtu.edu>
2017-05-14 16:23:30 -0400

    Added shadow to info box
    This contributes to #6

--- src/CSS.elm
+++ src/CSS.elm
@@ -2,4 +2,13 @@ module CSS exposing (..)
 
 
 box : List (String, String)
-box =  [("display", "inline-block"), ("padding", "5px"), ("border", "2px solid gray"), ("margin", "0"), ("background-color", "white"), ("white-space", "nowrap"), ("overflow", "hidden"), ("border-radius", "5px")]
+box = [ ("display", "inline-block")
+      , ("padding", "5px")
+      , ("border", "1px solid gray")
+      , ("margin", "0")
+      , ("background-color", "white")
+      , ("white-space", "nowrap")
+      , ("overflow", "hidden")
+      , ("border-radius", "2px")
+      , ("box-shadow", "2px 2px 6px gray")
+      ]
